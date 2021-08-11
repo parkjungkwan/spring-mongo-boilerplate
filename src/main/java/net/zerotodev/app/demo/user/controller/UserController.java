94f52d471066c0a98e556b7ba856af304c4e1787
parkjungkwan <pakjkwan@gmail.com>
2021-08-11 16:49:25 +0900

    17

--- src/main/java/net/zerotodev/app/demo/user/controller/UserController.java
+++ src/main/java/net/zerotodev/app/demo/user/controller/UserController.java
@@ -1,4 +1,27 @@
 package net.zerotodev.app.demo.user.controller;
 
-public class UserController {
+import lombok.RequiredArgsConstructor;
+import net.zerotodev.app.demo.user.entity.User;
+import net.zerotodev.app.demo.user.service.UserService;
+import net.zerotodev.app.demo.user.service.UserServiceImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController @RequiredArgsConstructor
+public final class UserController {
+    private final UserServiceImpl userService;
+
+    @GetMapping("/users/all")
+    public List<User> findAll(){
+        return userService.findAll();
+    }
+
+    @GetMapping("/users/name/{name}")
+    public List<User> findByName(@PathVariable String name){
+        return userService.findAllByName(name);
+    }
+
 }
--- src/main/java/net/zerotodev/app/demo/user/service/UserServiceImpl.java
+++ src/main/java/net/zerotodev/app/demo/user/service/UserServiceImpl.java
@@ -1,4 +1,21 @@
 package net.zerotodev.app.demo.user.service;
 
+import lombok.RequiredArgsConstructor;
+import net.zerotodev.app.demo.user.entity.User;
+import net.zerotodev.app.demo.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
+    private final UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+    public List<User> findAllByName(String name){
+        return userRepository.findAllByName(name);
+    }
 }
